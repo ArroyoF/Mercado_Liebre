3f7991fa439ac7008478b2592e091d4a58fd1ced
arflori <arroyoflorencia@gmail.com>
2022-08-29 20:36:17 -0300

    Procfile

--- app.js
+++ app.js
@@ -9,4 +9,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
-app.listen(3030)
+app.listen(process.env.PORT || 3030, function() {
+console.log('Servidor corriendo') 
+})
