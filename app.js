b05c26aa69250c4ea70fd5cfef9d63fc95795d8c
arflori <arroyoflorencia@gmail.com>
2022-08-31 16:05:59 -0300

    cambios login

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const app = express()
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.listen(process.env.PORT || 3030,() => {
+    console.log('Servidor corriendo') 
+    })
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
-app.listen(process.env.PORT || 3030, function() {
-console.log('Servidor corriendo') 
+app.get('/register', (req, res) =>{
+    res.sendFile(__dirname + '/views/register.html')
 })
+
+app.get('/login', (req, res) =>{
+    res.sendFile(__dirname + '/views/login.html')
+})
+
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
